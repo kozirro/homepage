255b85cb41b29a739d9413351ae0623c39fe59b0
Jakub Wójcik <kubaton1994@o2.pl>
2020-02-26 17:30:51 +0100

    updated css

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
 const kuba = 'kuba';
-const age = '26';
+const age = '25';
 
 let isHappy = false;
 isHappy = 'true';
 
 
 console.log(` Nazywam się ${kuba} i mam ${age} lat`);
+
+
+const box = document.querySelector('.box-test');
+box.innerHTML = "zabawa";
+console.log(box.innerHTML)
+
+const paragraphs = document.querySelectorAll('p');
+
+console.log(paragraphs[0]);
